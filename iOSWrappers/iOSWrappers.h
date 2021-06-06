a6a4d4d6d8c34b7948783941835bcc3aa65937d6
Chaitanya <chaitanyab207@gmail.com>
2021-06-06 16:27:11 +0530

    Empty template for iOS framework

---
+++ iOSWrappers/iOSWrappers.h
@@ -0,0 +1,18 @@
+//
+//  iOSWrappers.h
+//  iOSWrappers
+//
+//  Created by Kala on 06/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSWrappers.
+FOUNDATION_EXPORT double iOSWrappersVersionNumber;
+
+//! Project version string for iOSWrappers.
+FOUNDATION_EXPORT const unsigned char iOSWrappersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSWrappers/PublicHeader.h>
+
+
